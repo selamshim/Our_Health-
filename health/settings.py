8cfd94c9f155a6111c93c8aa650b913ded1f1d09
Selam Yigezu <shimeselam@gmail.com>
2024-06-28 10:52:53 +0000

    secreat key is addand configured

--- health/settings.py
+++ health/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gx5g!0f$u4%!5dofd!aea%uc@&re!-$jky6@49#n2$22egi1#j'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
