7042185ebf424cd468112034a7dbada5eaac2d4c
Selam Yigezu <shimeselam@gmail.com>
2024-07-02 04:19:16 +0000

    models are added

--- post/models.py
+++ post/models.py
@@ -13,4 +13,25 @@ class Issue(models.Model):
     HealthCase = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"    
 
+class Comment(models.Model):
+    Issue = models.ForeignKey(
+        Issue, on_delete=models.CASCADE, related_name="comments")
+    Patient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"    
