43555eb4fd5c7fbe6d3baca4d9101238abcc9c63
Selam Yigezu <shimeselam@gmail.com>
2024-07-07 01:53:52 +0000

    view edited

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://u4jezo2k56l:Z2MJJ5SA7Vh0@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/pace_food_delay_244668")
+os.environ.setdefault("SECRET_KEY", "MikaAmanSeliDave")
